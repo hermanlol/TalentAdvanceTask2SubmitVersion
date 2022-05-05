31abd40b94d743e5112d441cd34dcd0c5dbf93b5
hermanlol <57663389+hermanlol@users.noreply.github.com>
2022-05-05 23:07:44 +1200

    Add files via upload

---
+++ Models/Skill.cs
@@ -0,0 +1,19 @@
+﻿using Talent.Common.Contracts;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Talent.Common.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Skill : IMongoCommon
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
